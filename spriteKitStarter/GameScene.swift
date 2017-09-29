adb0f63a2965308a7d6892045c34696ac76d2067
cdills <dillscody@gmail.com>
2017-09-29 14:41:54 -0400

    lets get physical

--- spriteKitStarter/GameScene.swift
+++ spriteKitStarter/GameScene.swift
@@ -9,6 +9,17 @@
 import SpriteKit
 import GameplayKit
 
+// SpriteKite Category is a 32 bit integer. Each of the bits represents a single category allowing for 32 total
+// This struct sets first bit to "Monster" and second bit to "projectile"
+////Do All and None count towards the 32 cap? How can I exceed the limit in one scene?
+struct PhysicsCategory {
+    static let None : UInt32 = 0
+    static let All  : UInt32 = UInt32.max
+    static let Monster : UInt32 = 0b1
+    static let Projectile : UInt32 = 0b10
+}
+
+
 // Pre-Built Vector Math Functions
 // I should learn just what this "Vector Math" is anyway
 
@@ -44,7 +55,7 @@ extension CGPoint {
     }
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //1 Declare constant sprite, pass image name
     let player = SKSpriteNode(imageNamed: "player")
@@ -56,6 +67,9 @@ class GameScene: SKScene {
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         //4 draws node on screen
         addChild(player)
+        
+        physicsWorld.gravity = CGVector.zero //Sets our "PHYSICS WORLD" to be gravity free
+        physicsWorld.contactDelegate = self // sets the scene as deleegate to be notified when contact is made between bodies
         
         run(SKAction.repeatForever(SKAction.sequence([SKAction.run(addMonster),
             SKAction.wait(forDuration: 1.0)
@@ -82,6 +96,13 @@ class GameScene: SKScene {
         
         //add to scene
         addChild(monster)
+        //Do physics
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) //Physicsbody = hitbox. uses rectangle here to approximate. SUPER USEFUL
+        monster.physicsBody?.isDynamic = true //this means physics engine WILL NOT contol the movement. we will control it via move actions
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster //sets category
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile //chooses categories of objects this objects should send contact notifications for to delegate. it "test" for Contact
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None //which objects should this object collide with / Physics controlled collisions (bouncing) / Set to none so they passthrough
+        
         
         //calclulate speed
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
@@ -106,6 +127,15 @@ class GameScene: SKScene {
         // Set initial location of projection
         let projectile = SKSpriteNode(imageNamed: "projectile") //Reminder: this creates a sprite constant with the image specified
         projectile.position = player.position //because we want to shoot from the player
+        
+        //do physics for projectile now
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2) //set hitbox to approximate circle.
+        projectile.physicsBody?.isDynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true //Set on fast moving bodies to make sure it happens. Why not always set? Precise is Nice
+        
         
         //Determine offset for projectile
         let offset = touchLocation - projectile.position
@@ -135,9 +165,36 @@ class GameScene: SKScene {
         
     }
         
+    func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+        print("GET EEEEM")
+        projectile.removeFromParent()
+        monster.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        //this part sorts the colliding bodies by their order in the category list. This should simplify calculations if categories ordered properly
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
         
-    
-    
-    
+        //first uses bitwise & operator to check if the bodies are monster and projectile. If the & function returns 0 then BodycategoryBitMask does not match physicsCategory
+        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0 ) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+            //this part creates skspritenode constants to pass to our did collide function
+          if let monster = firstBody.node as? SKSpriteNode,
+             let projectile = secondBody.node as? SKSpriteNode {
+             projectileDidCollideWithMonster(projectile: projectile, monster: monster)
+            }
+            
+        }
+        
+        
+        }
     
 }
